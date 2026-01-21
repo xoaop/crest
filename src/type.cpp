da7c1ab0be9c56d59e971c8deba5367daca1e22f
xoaop <1960967250@qq.com>
2026-01-21 18:30:07 +0800

    没啥

--- src/type.cpp
+++ src/type.cpp
@@ -295,7 +295,6 @@ bool is_integer_type(TypeRef type) {
     case Type_u8:
     case Type_u32:
     case Type_u64:
-    // case Type_bool: // NOTE: bool 也作为整数类型处理, i1
         return true;
     default:
         return false;
@@ -303,19 +302,7 @@ bool is_integer_type(TypeRef type) {
 }
 
 bool is_integer_or_bool_type(TypeRef type) {
-    switch(type->kind)
-    {
-    case Type_i8:
-    case Type_i32:
-    case Type_i64:
-    case Type_u8:
-    case Type_u32:
-    case Type_u64:
-    case Type_bool: // NOTE: bool 也作为整数类型处理, i1
-        return true;
-    default:
-        return false;
-    }
+    return is_integer_type(type) || type->kind == Type_bool;
 }
 
 bool is_signed_type(TypeRef type) {
@@ -332,15 +319,7 @@ bool is_signed_type(TypeRef type) {
 
 bool is_signed_or_bool_type(TypeRef type) {
     XP_ASSERT_DEFAULT(is_integer_or_bool_type(type));
-    switch(type->kind) {
-    case Type_i8:
-    case Type_i32:
-    case Type_i64:
-    case Type_bool:
-        return true;
-    default:
-        return false;
-    }
+    return is_signed_type(type) || type->kind == Type_bool;
 }
 
 bool is_unsigned_type(TypeRef type) {
@@ -350,7 +329,6 @@ bool is_unsigned_type(TypeRef type) {
     case Type_u32:
     case Type_u64:
 
-    // case Type_bool: // NOTE: bool 也作为无符号整数类型处理, i1
         return true;
     default:
         return false;
