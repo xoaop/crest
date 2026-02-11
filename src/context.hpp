2aa51d2345edf85af9d37d1f288ddc3ae879f834
xoaop <1960967250@qq.com>
2026-02-11 11:27:34 +0800

    初步添加error_reporter

--- src/context.hpp
+++ src/context.hpp
@@ -2,13 +2,14 @@
 #define CREST_CONTEXT_HPP
 
 #include "package.hpp"
+#include "error_msg.hpp"
 
 struct Context {
-    // Scope global_scope;
-
     const char *main_src_dir_path;
 
     Package global_blank_package;
+
+    ErrorReporter reporter;
 };
 
 Context *context();
