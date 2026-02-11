ae7ab31a9d1e493aed0a1ee6e2aea80e314617c4
xoaop <1960967250@qq.com>
2026-02-11 11:25:38 +0800

    添加merge

--- src/span.cpp
+++ src/span.cpp
@@ -7,3 +7,22 @@ Span make_span(isize start, isize end) {
 
     return span;
 }
+
+
+Span merge(Span a, Span b) {
+    Span merged = {};
+    
+    if(a.start < b.start) {
+        merged.start = a.start;
+    } else {
+        merged.start = b.start;
+    }
+
+    if(a.end > b.end) {
+        merged.end = a.end;
+    } else {
+        merged.end = b.end;
+    }
+
+    return merged;
+}
