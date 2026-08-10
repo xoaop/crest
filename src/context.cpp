19281ccc615885326e5e130fe21b1921cd4a75ff
xoaop <1960967250@qq.com>
2026-08-11 00:14:12 +0800

    packageref， 没了

--- src/context.cpp
+++ src/context.cpp
@@ -5,3 +5,10 @@ static Context global_context;
 Context *context() {
     return &global_context;
 }
+
+
+
+PackageRef add_package(Context *ctx, Package pkg) {
+    ctx->all_packages.push_back(std::move(pkg));
+    return ctx->all_packages.count - 1;
+}
